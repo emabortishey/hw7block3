a2c4b21450e26c2b048a4a646cea4f52ae79f9d9
abortish <fartbreather228@gmail.com>
2024-10-26 06:11:50 +0300

    создана новая ветка и добавлены перегрузки операторов в обе из них

---
+++ Flat.h
@@ -0,0 +1,27 @@
+#pragma once
+#include <iostream>
+using namespace std;
+
+class Flat
+{
+	int area;
+	int cost;
+public:
+	Flat() : Flat(0, 0) {}
+	explicit Flat(int area_P) : Flat(area_P, 0) {}
+	Flat(int area_P, int cost_P) : cost{ cost_P }, area{ area_P } { }
+	Flat(const Flat& obj) : cost{ obj.cost }, area{ obj.area } { }
+
+	void set_area(int area_P) { area = area_P; }
+	void set_cost(int cost_P) { cost = cost_P; }
+
+	Flat& operator=(const Flat& obj) { area = obj.area; cost = obj.cost; return *this; }
+
+	friend bool operator==(const Flat& obj, const Flat& obj2) { return (obj.area == obj2.area); }
+	friend bool operator>(const Flat& obj, const Flat& obj2) { return (obj.cost > obj2.cost); }
+	friend bool operator<(const Flat& obj, const Flat& obj2) { return (obj.cost < obj2.cost); }
+
+	int get_area() { return area; }
+	int get_cost() { return cost; }
+};
+
--- Overcoat.h
+++ Overcoat.h
@@ -15,7 +15,19 @@ public:
 	void set_type(char* type_P) { delete[] type; type = new char[strlen(type_P) + 1] {*type_P}; }
 	void set_cost(int cost_P) { cost = cost_P; }
 
-	
+	Overcoat& operator=(const Overcoat& obj)
+	{
+		delete[] type;
+		type = { new char[strlen(obj.type) + 1] };
+		strcpy_s(type, strlen(obj.type) + 1, obj.type);
+		cost = obj.cost;
+		return *this;
+	}
+
+	friend bool operator==(const Overcoat& obj, const Overcoat& obj2) { return (strcmp(obj.type, obj2.type) == 0); }
+	friend bool operator>(const Overcoat& obj, const Overcoat& obj2) { if (obj == obj2) { return (obj.cost > obj2.cost); } return 404; }
+	friend bool operator<(const Overcoat& obj, const Overcoat& obj2) { if (obj == obj2) { return (obj.cost < obj2.cost); } return 404; }
+
 	const char* get_type() { return type; }
 	int get_cost() { return cost; }
 };
